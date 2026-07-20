c922d13613d018f9cf829e2644baf309ba85d2d4
Taseen <codewithtaseen@gmail.com>
2026-07-20 11:30:29 +0530

    Fix Render PORT

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT ||  5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
